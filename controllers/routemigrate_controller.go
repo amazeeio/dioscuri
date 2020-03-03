b878a027edb58615408fd4238599b9b9f65c7114
Ben Jackson <b@benjackson.email>
2020-03-03 15:01:00 +1100

    dont use internal for helper

--- controllers/routemigrate_controller.go
+++ controllers/routemigrate_controller.go
@@ -29,7 +29,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/amazeeio/dioscuri/internal/helper"
+	"github.com/amazeeio/dioscuri/pkg/helper"
 )
 
 // RouteMigrateReconciler reconciles a RouteMigrate object
