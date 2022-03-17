0cdc5bb53f1b00f5dada1ab71ab3eadff2045d66
Ben Jackson <b@benjackson.email>
2022-03-17 17:56:32 +1100

    chore: more openshift tidying up

--- main.go
+++ main.go
@@ -51,7 +51,6 @@ func init() {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
-	var openShift bool
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
